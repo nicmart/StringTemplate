6a0bb008e3844626bf9a9872ae4098469684fe2a
Nicolò Martini <electroportal.net@gmail.com>
2013-08-14 15:25:14 +0200

    Engine class implementation

--- src/StringTemplate/Engine.php
+++ src/StringTemplate/Engine.php
@@ -12,8 +12,46 @@ namespace StringTemplate;
 
 /**
  * Class Engine
+ *
+ * Replace placeholder in strings with nested (array) values.
+ *
+ * Example:
+ * <code>
+ * $engine->render('This is {a} and these are {c.0} and {c.1}', ['a' => 'b', 'c' => ['d', 'e']]);
+ * //Prints "This is b and these are d and e"
+ * </code>
  */
 class Engine
 {
+    protected $left;
+    protected $right;
 
+    /**
+     * @param string $left  The left delimiter
+     * @param string $right The right delimiter
+     */
+    public function __construct($left = '{', $right = '}')
+    {
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    /**
+     * @param string $template      The template string
+     * @param string|array $value   The value the template will be rendered with
+     *
+     * @return string The rendered template
+     */
+    public function render($template, $value)
+    {
+        $result = $template;
+        if (!is_array($value))
+            $value = array('' => $value);
+
+        foreach (new NestedKeyArrayIterator($value) as $key => $value) {
+            $result = str_replace($this->left . $key . $this->right, $value, $result);
+        }
+
+        return $result;
+    }
 }
